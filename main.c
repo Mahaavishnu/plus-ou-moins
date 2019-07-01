65462e5339c8c76b892c31a08342df209dfebcc0
Mahaavishnu <casubolo.matthieu@gmail.com>
2019-07-02 00:05:51 +0200

    implementation compteur de coups

--- main.c
+++ main.c
@@ -8,9 +8,11 @@ int		main(void)
 {
 	int	mystery;
 	int	guess;
+	int	tries;
 
 	srand(time(NULL));
 	mystery = (rand() % (MAX - MIN + 1)) + MIN;
+	tries = 0;
 	do
 	{
 		printf("Quel est le nombre ? ");
@@ -19,9 +21,10 @@ int		main(void)
 			printf("C'est moins !\n");
 		else if (mystery > guess)
 			printf("C'est plus !\n");
+		tries++;
 	} while (mystery != guess);
 	
-	printf("\nBravo, vous avez trouvé le nombre mystère !\n");
+	printf("\nBravo, vous avez trouvé le nombre mystère en %d coups !\n", tries);
 
 	return (0);
 }
