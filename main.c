1202a69ad2af4c29f0c9a46c9173c731019bddee
Mahaavishnu <casubolo.matthieu@gmail.com>
2019-07-02 00:36:47 +0200

    ajout du mode 2 joueurs et niveau de difficulté

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <time.h>
 
-#define MAX 100
 #define MIN 1
 
 int		main(void)
@@ -10,11 +9,34 @@ int		main(void)
 	int	guess;
 	int	tries;
 	int	replay;
+	int	players;
+	int	mode;
+	int	max;
 
 	srand(time(NULL));
 	do
 	{
-		mystery = (rand() % (MAX - MIN + 1)) + MIN;
+		printf ("Veuillez choisir un mode de difficulté. 1, 2 ou 3. ");
+		scanf ("%d", &mode);
+		if (mode == 1)
+			max = 100;
+		else if (mode == 2)
+			max = 1000;
+		else
+			max = 10000;
+		printf("Voulez-vous jouer en mode 1 ou 2 joueurs ? ");
+		scanf("%d", &players);
+		if (players == 1)
+			mystery = (rand() % (max - MIN + 1)) + MIN;
+		else
+		{
+			do
+			{
+				printf("Joueur 1 - Veuillez entrer le nombre mystère à deviner, compris entre %d et %d. ", MIN, max);
+				scanf("%d", &mystery);
+			} while (mystery < MIN || mystery > max);
+			printf("Joueur 2 - A vous de jouer\n");
+		}
 		tries = 0;
 		do
 		{
