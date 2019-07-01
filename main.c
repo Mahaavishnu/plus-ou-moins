46c1ad4e3db8c341df84bad8fe9680e665cf7f3e
Mahaavishnu <casubolo.matthieu@gmail.com>
2019-07-02 00:11:23 +0200

    implementation option rejouer une partie

--- main.c
+++ main.c
@@ -9,22 +9,27 @@ int		main(void)
 	int	mystery;
 	int	guess;
 	int	tries;
+	int	replay;
 
 	srand(time(NULL));
-	mystery = (rand() % (MAX - MIN + 1)) + MIN;
-	tries = 0;
 	do
 	{
-		printf("Quel est le nombre ? ");
-		scanf("%d", &guess);
-		if (mystery < guess)
-			printf("C'est moins !\n");
-		else if (mystery > guess)
-			printf("C'est plus !\n");
-		tries++;
-	} while (mystery != guess);
-	
-	printf("\nBravo, vous avez trouvé le nombre mystère en %d coups !\n", tries);
+		mystery = (rand() % (MAX - MIN + 1)) + MIN;
+		tries = 0;
+		do
+		{
+			printf("Quel est le nombre ? ");
+			scanf("%d", &guess);
+			if (mystery < guess)
+				printf("C'est moins !\n");
+			else if (mystery > guess)
+				printf("C'est plus !\n");
+			tries++;
+		} while (mystery != guess);
+		
+		printf("\nBravo, vous avez trouvé le nombre mystère en %d coups !\nVoulez-vous rejouer ? 0 ou 1. ", tries);
+		scanf("%d", &replay);
+	} while (replay == 1);
 
 	return (0);
 }
