63044d1dd8023932966ac3906a9ca17f732efdaa
Mahaavishnu <casubolo.matthieu@gmail.com>
2019-07-02 00:01:09 +0200

    version fonctionnelle sans option

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <time.h>
+
+#define MAX 100
+#define MIN 1
 
 int		main(void)
 {
-	printf("Hello world");
+	int	mystery;
+	int	guess;
+
+	srand(time(NULL));
+	mystery = (rand() % (MAX - MIN + 1)) + MIN;
+	do
+	{
+		printf("Quel est le nombre ? ");
+		scanf("%d", &guess);
+		if (mystery < guess)
+			printf("C'est moins !\n");
+		else if (mystery > guess)
+			printf("C'est plus !\n");
+	} while (mystery != guess);
+	
+	printf("\nBravo, vous avez trouvé le nombre mystère !\n");
 
 	return (0);
 }
